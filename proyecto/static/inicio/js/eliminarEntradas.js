16fc159974c3236a6ea3856174951911af02f1a3
Simon <sabh20020509@gmail.com>
2023-11-18 17:05:13 -0400

    PRUEBAAAA

--- proyecto/static/inicio/js/eliminarEntradas.js
+++
@@ -1,8 +0,0 @@
-function eliminarEntrada(idEntrada, modo)
-{
-
-    $("#confirm-modal").modal('show')
-    let borrar = document.getElementById('modal_borrar');
-    borrar.href = "/inventario/eliminar/" + modo + "/" + idEntrada
-
-} 
