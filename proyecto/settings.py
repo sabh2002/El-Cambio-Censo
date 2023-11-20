0b8bad9cd0a5eda681f398661ffc117ba71e4111
Simon <sabh20020509@gmail.com>
2023-11-20 18:49:23 -0400

    Render otra vezz

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -137,12 +137,13 @@ STATIC_URL = 'static/'
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'proyecto/static')]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'proyecto/static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
